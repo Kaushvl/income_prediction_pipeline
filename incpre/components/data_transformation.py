34e03a2a69599f4e76ca827b9d27994b3390c9ac
kaushal <kaushalbro1@gmail.com>
2023-07-18 19:04:13 +0530

    model trainer added

--- incpre/components/data_transformation.py
+++ incpre/components/data_transformation.py
@@ -122,12 +122,3 @@ class DataTransformation:
 
         except Exception as e:
             raise CustmeException(e, sys)
-        
-
-# if __name__ == '__main__':
-
-#     obj = DataIngestion()
-#     train_path, test_path = obj.initiate_data_ingestion()
-
-#     data_transformation = DataTransformation()
-#     train_arr, test_arr , _ = data_transformation.inititate_data_transformation(train_path, test_path)
